c1acefab1a160024f9b680a617b51655584e6421
Vlad <vladdosiik7540@gmail.com>
2020-02-03 13:07:11 +0300

    fixed bugs

--- app/src/main/java/biz/bixer/bixer/Splash/SplashScreen.java
+++ app/src/main/java/biz/bixer/bixer/Splash/SplashScreen.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import biz.bixer.bixer.MainActivity;
+import biz.bixer.bixer.Pages.MainActivity;
 import biz.bixer.bixer.R;
 
 public class SplashScreen extends Activity {
