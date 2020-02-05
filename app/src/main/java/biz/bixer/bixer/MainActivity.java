da4b7b4d55cf9d9550395de06678a7f91e250b3d
Vlad <vladdosiik7540@gmail.com>
2020-02-05 13:49:56 +0300

    fixed bugs

--- app/src/main/java/biz/bixer/bixer/MainActivity.java
+++ app/src/main/java/biz/bixer/bixer/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity
         drawer.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.tvBackground));
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-       
+
     }
     private static long back_pressed;
     @Override
