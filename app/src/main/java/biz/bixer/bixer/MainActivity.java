0fec908501654f4d2314d46a564cb409f90b39d3
Vlad <vladdosiik7540@gmail.com>
2020-05-23 16:32:53 +0300

    update

--- app/src/main/java/biz/bixer/bixer/MainActivity.java
+++ app/src/main/java/biz/bixer/bixer/MainActivity.java
@@ -72,9 +72,7 @@ public class MainActivity extends AppCompatActivity
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         Fragment fragment = null;
         Class fragmentClass = null;
         int id = item.getItemId();
