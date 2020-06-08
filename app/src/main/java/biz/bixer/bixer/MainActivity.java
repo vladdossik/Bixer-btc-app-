099f920f2d7a3b3bc848b1389e8e63e218e49fb7
Vladdosiik <vladdosiik7540@gmail.com>
2020-06-08 19:39:13 +0300

    update

--- app/src/main/java/biz/bixer/bixer/MainActivity.java
+++ app/src/main/java/biz/bixer/bixer/MainActivity.java
@@ -67,7 +67,6 @@ public class MainActivity extends AppCompatActivity
         Fragment fragment = null;
         Class fragmentClass = null;
         int id = item.getItemId();
-        //noinspection SimplifiableIfStatement
         if(id==R.id.action_website)
         {
             fragmentClass= website.class;
