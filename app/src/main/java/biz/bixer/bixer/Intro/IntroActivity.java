738ad9a03d8ea90bb1c76540a42b989de2fe2e16
Vladdosiik <vladdosiik7540@gmail.com>
2020-06-26 09:18:22 +0300

    update

--- app/src/main/java/biz/bixer/bixer/Intro/IntroActivity.java
+++ app/src/main/java/biz/bixer/bixer/Intro/IntroActivity.java
@@ -35,9 +35,7 @@ public class IntroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // make the activity on full screen
-
+        
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
