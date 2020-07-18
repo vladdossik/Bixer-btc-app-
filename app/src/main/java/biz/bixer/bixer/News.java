44864518c2fb9737a3dbabec54d6ee5bfa0b70f7
Vladdosiik <vladdosiik7540@gmail.com>
2020-07-18 12:06:54 +0300

    .

---
+++ app/src/main/java/biz/bixer/bixer/News.java
@@ -0,0 +1,13 @@
+package biz.bixer.bixer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class News extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+    }
+}
